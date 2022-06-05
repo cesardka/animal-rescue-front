a38f1bf7bb7ded7c0c9619c14c252340b045decd
Vanessa Schenkel <v.s.schenkel@gmail.com>
2022-06-05 12:56:41 -0300

    nav e contrato

--- src/App.js
+++ src/App.js
@@ -1,7 +1,111 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Routes,
+} from "react-router-dom";
 import { Login } from "./pages/login";
+import { Contrato } from "./pages/contrato";
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/sobre'>
+              Sobre
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/contrato'>
+              Contrato
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/perfil'>
+              Perfil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/meus-animais'>
+              Meus animais
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/doar'>
+              Doar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/adotar'>
+              Adotar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) =>
+                navData.isActive ? "active-style" : "none"
+              }
+              to='/login'>
+              Login
+            </NavLink>
+          </li>
+        </nav>
 
-function App() {
-  return <Login />;
+        {/* 👇️ Wrap your Route components in a Routes component */}
+        <Routes>
+          <Route path='/sobre' element={<About />} />
+          <Route path='/contrato' element={<Contrato />} />
+          <Route path='/perfil' element={<About />} />
+          <Route path='/meus-animais' element={<About />} />
+          <Route path='/doar' element={<About />} />
+          <Route path='/adotar' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}

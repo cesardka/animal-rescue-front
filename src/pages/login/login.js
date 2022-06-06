819d423a53184da5c13cd3a5feccb6ac79d6b710
Vanessa Schenkel <v.s.schenkel@gmail.com>
2022-06-06 20:43:22 -0300

    login sem cors

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -3,25 +3,34 @@ import { PrimaryButton, SecondaryButton } from "../../components/Button";
 import { PasswordInput, TextInput } from "../../components/Input";
 import { FormWrapper } from "./styles";
 
+import axios from "axios";
+
 export const TelaLogin = ({ irParaTela = () => {}, ...props }) => {
   const emailLogin = useRef("");
   const passwordLogin = useRef("");
 
   const handleSubmitLogin = (e) => {
     e.preventDefault();
+
+    axios.post("http://localhost:8000/login_register/login/", {
+      email: "gkdalcin@gmail.com",
+      password: "sngahdr4g3",
+    });
+
+    //URL: https://animal-rescue-project.herokuapp.com/login_register/login/
   };
 
   return (
     <FormWrapper onSubmit={(e) => handleSubmitLogin(e)}>
       <TextInput
-        label="E-mail"
-        name="email"
+        label='E-mail'
+        name='email'
         ref={emailLogin}
         value={emailLogin.current.value}
       />
       <PasswordInput
-        label="Senha"
-        name="senha"
+        label='Senha'
+        name='senha'
         ref={passwordLogin}
         value={passwordLogin.current.value}
       />

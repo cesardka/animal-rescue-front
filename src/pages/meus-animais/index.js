cd95ae9c4839838ae8d279acc46b1e85f92b55bb
Vanessa Schenkel <v.s.schenkel@gmail.com>
2022-06-07 19:59:55 -0300

    animais arrumado

--- src/pages/meus-animais/index.js
+++ src/pages/meus-animais/index.js
@@ -8,9 +8,10 @@ export function MeusAnimais() {
   const [allAnimals, setStateAllAnimals] = useState([]);
   const [isReadOnly, setIsReadOnly] = useState(true);
   const [oldName, setOldName] = useState("");
+  const [oldPic, setOldPic] = useState("");
   const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState("");
   const [newDescription, setNewDescription] = useState("");
-  const [newIdade, setNewIdade] = useState("");
 
   const getAnimals = async () => {
     const email = localStorage.getItem("user_email");
@@ -22,14 +23,21 @@ export function MeusAnimais() {
       }
     );
 
+    animals.data.forEach((ani) => {
+      ani.imgUrl = getRandomPic();
+    });
+
     return animals.data;
   };
 
   const getAllMyAnimals = async () => {
     const animal_mock = animaisData;
+
     const animal_data = await getAnimals();
 
-    const all = [...animal_mock, ...animal_data];
+    const all = [...animal_data, ...animal_mock];
+
+    console.log(all);
 
     setStateAllAnimals(all);
   };
@@ -40,7 +48,10 @@ export function MeusAnimais() {
 
   const handleDelete = (name) => {
     const email = localStorage.getItem("user_email");
-    console.log("usuario", email);
+
+    const newList = allAnimals.filter((item) => item.name !== name);
+
+    setStateAllAnimals(newList);
 
     axios.post(
       "https://animal-rescue-project.herokuapp.com/pet_adoption/delete-pet/",
@@ -49,55 +60,72 @@ export function MeusAnimais() {
         user_email: email,
       }
     );
-
-    console.log("nome para deletar", name);
   };
 
   const handleEdit = async (data) => {
     setOldName(data.name);
+    setNewAge(data.idade);
+    setNewDescription(data.description);
+    setNewName(data.name);
+    setOldPic(data.imgUrl);
 
     setIsReadOnly(false);
-
-    console.log(data);
   };
 
-  const handleSaveEditions = async (data) => {
-    const email = localStorage.getItem("user_email");
+  const handleAge = (age) => {
+    const today = new Date();
+    const birth = new Date(age);
 
-    await axios.patch(
-      "https://animal-rescue-project.herokuapp.com/pet_adoption/register_pet/",
-      {
-        user_email: email,
-        name: data.name,
-        new_name: "Rex",
-        new_description: data.description,
-        new_idade: data.idade,
-      }
+    return Math.floor(
+      Math.ceil(
+        Math.abs(birth.getTime() - today.getTime()) / (1000 * 3600 * 24)
+      ) / 365.25
     );
   };
 
-  return (
-    <div className='container'>
-      <div className='meus-animais'>
+  const handleSaveEditions = async () => {
+    const email = localStorage.getItem("user_email");
+
+    const age_in_years = handleAge(newAge);
+
+    const retorno = await axios.patch(
+      "https://animal-rescue-project.herokuapp.com/pet_adoption/register_pet/",
+      {
+        user_email: email,
+        name: oldName,
+        new_name: newName,
+        new_description: newDescription,
+        new_idade: age_in_years,
+      }
+    );
+
+    console.log(retorno);
+  };
+
+  const getRandomPic = () => {
+    const urls = [
+      "https://live-production.wcms.abc-cdn.net.au/ff1221fbfdb2fe163fdda15df5f77676?impolicy=wcms_crop_resize&cropH=394&cropW=700&xPos=0&yPos=37&width=862&height=485",
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+      "https://images.unsplash.com/photo-1587402092301-725e37c70fd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3V0ZSUyMGRvZ3N8ZW58MHx8MHx8&w=1000&q=80",
+      "https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=iQEwihUn",
+    ];
+    const randomPic = urls[Math.floor(Math.random() * urls.length)];
+
+    return randomPic;
+  };
+
+  const showInfos = () => {
+    return (
+      <>
         <div className='title-pag'>Para adoção</div>
         {allAnimals.map((data, key) => {
           return (
             <div key={key} className='animal'>
-              <img
-                src={
-                  data.imgUrl ||
-                  "https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=iQEwihUn"
-                }
-              />
+              <img src={data.imgUrl} />
               <h3 className='title'>{data.name}</h3>
               <div className='infos'>
                 <div className='info-esquerda'>
-                  <div className='info'>
-                    <input
-                      readOnly={isReadOnly}
-                      value={isReadOnly ? data.name : newName}
-                    />
-                  </div>
                   <div className='info'>Espécie: {data.especie}</div>
                   <div className='info'>Descrição: {data.description}</div>
                 </div>
@@ -123,6 +151,60 @@ export function MeusAnimais() {
         <div className='adotados'>
           Você ainda não adotou nenhum animal pelo nosso site.
         </div>
+      </>
+    );
+  };
+
+  const showEdit = () => {
+    return (
+      <>
+        <div className='animal'>
+          <form>
+            <div className='infos-edit'>
+              <img src={oldPic} />
+              <label>Nome do animal</label>
+              <input
+                type='text'
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+              />
+
+              <label>Descrição</label>
+              <textarea
+                type='text'
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+              />
+
+              <label>Data de nascimento aproximado</label>
+              <input
+                value={newAge}
+                onChange={(e) => setNewAge(e.target.value)}
+                type='date'
+                id='start'
+                name='trip-start'
+                min='2018-01-01'
+                max='2022-12-31'
+              />
+            </div>
+            <div className='div-salvar'>
+              <button
+                type='submit'
+                className='button-salvar'
+                onClick={() => handleSaveEditions()}>
+                Salvar
+              </button>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className='container'>
+      <div className='meus-animais'>
+        {isReadOnly ? showInfos() : showEdit()}
       </div>
     </div>
   );
